d752db6b86fca537deac1f1be867c1a2fc28b79e
farm <lukas_karotta@hotmail.com>
2018-08-18 17:50:22 -0300

    Fixing empty aka character's name.

--- src/lib/anilist/formatting/characters.ts
+++ src/lib/anilist/formatting/characters.ts
@@ -17,16 +17,19 @@ export const charactersImage = ({ medium, large }: CharacterImage): string => {
 export const charactersAllNames = ({ name, translation }: CharactersNameContext): CharactersNameResponse => {
     const { native, first, last, alternative } = name;
     let complete = '';
+    let aka = false;
     
     if (null !== first) {
         complete += first;
     } if (null !== last) {
         complete += ' ' + last;
+    } if (null !== alternative && '' !== alternative[0]) {
+        aka = true;
     }
 
     return {
         native: (null !== native) ? translation.t('native', { native }) : '',
         name: ('' !== complete) ? translation.t('name', { name: complete }) : '',
-        alternative: (null !== alternative) ? translation.t('alternative', { alternative: joining(alternative) }) : ''
+        alternative: (true === aka) ? translation.t('alternative', { alternative: joining(alternative) }) : ''
     };
 };
